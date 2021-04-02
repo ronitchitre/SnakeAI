8b945ad1fc23f294f6ae7b68bc53a1c906048681
Ronit Chitre <chitreronit@gmail.com>
2021-04-02 11:17:49 +0530

    finished adding graphics

--- classes.py
+++ classes.py
@@ -1,28 +1,113 @@
 import pygame
+import colors
 from pygame.math import Vector2
+from random import randint
+
+cell_size = 20
+cell_number = 30
 
 
-class food:
-    def __init(self):
-        self.x = 5
-        self.y = 9
+class Food:
+    def __init__(self):
+        self.x = randint(0, cell_number - 1)
+        self.y = randint(0, cell_number - 1)
         self.pos = Vector2(self.x, self.y)
+        self.pic = pygame.image.load("food.png", ).convert_alpha()
+
+    def draw_food(self, gameWindow):
+        food_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        gameWindow.blit(self.pic, food_rect)
 
 
 class Snake:
     def __init__(self):
-        self.pos = [450, 300]
-        self.side = 30
-        self.vel_dir = [0, 0]
-        self.vel_mag = 5
-        self.turn = "0"
-        self.body = [[self.pos[0], self.pos[1], self.side, self.side, self.vel_dir, self.turn]]
-        self.length = 10
-        self.len_increment = 500
-        self.headpic = pygame.image.load("snakehead_up.png").convert_alpha()
-        self.body_hor = pygame.image.load("snake_bod_hor.png").convert_alpha()
-        self.body_ver = pygame.image.load("snake_bod_ver.png").convert_alpha()
-        self.body_corner4 = pygame.image.load("snake_corner_4.png").convert_alpha()
-        self.body_corner3 = pygame.image.load("snake_corner_3.png").convert_alpha()
-        self.body_corner2 = pygame.image.load("snake_corner_2.png").convert_alpha()
-        self.body_corner1 = pygame.image.load("snake_corner_1.png").convert_alpha()
+        self.body = [Vector2(15, 15), Vector2(15, 16), Vector2(15, 17)]
+        self.direction = Vector2(0, 0)
+        self.head_up = pygame.image.load("snakehead_up.png").convert_alpha()
+        self.head_down = pygame.image.load("snakehead_down.png").convert_alpha()
+        self.head_left = pygame.image.load("snakehead_left.png").convert_alpha()
+        self.head_right = pygame.image.load("snakehead_right.png").convert_alpha()
+
+        self.bod_ver = pygame.image.load("snake_bod_ver.png").convert_alpha()
+        self.bod_hor = pygame.image.load("snake_bod_hor.png").convert_alpha()
+
+        self.tail_up = pygame.image.load("snake_tail_-y.png").convert_alpha()
+        self.tail_down = pygame.image.load("snake_tail_+y.png").convert_alpha()
+        self.tail_left = pygame.image.load("snake_tail_-x.png").convert_alpha()
+        self.tail_right = pygame.image.load("snake_tail_+x.png").convert_alpha()
+
+        self.snake_corner1 = pygame.image.load("snake_corner_1.png").convert_alpha()
+        self.snake_corner2 = pygame.image.load("snake_corner_2.png").convert_alpha()
+        self.snake_corner3 = pygame.image.load("snake_corner_3.png").convert_alpha()
+        self.snake_corner4 = pygame.image.load("snake_corner_4.png").convert_alpha()
+
+        self.head = pygame.image.load("snakehead_up.png").convert_alpha()
+        self.tail = pygame.image.load("snake_tail_-y.png").convert_alpha()
+
+    def draw_snake(self, gameWindow):
+        self.update_head()
+        self.update_tail()
+        for index, block in enumerate(self.body):
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            if index == 0:
+                gameWindow.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:
+                gameWindow.blit(self.tail, block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    gameWindow.blit(self.bod_ver, block_rect)
+                elif previous_block.y == next_block.y:
+                    gameWindow.blit(self.bod_hor, block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        gameWindow.blit(self.snake_corner4, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        gameWindow.blit(self.snake_corner3, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        gameWindow.blit(self.snake_corner1, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        gameWindow.blit(self.snake_corner2, block_rect)
+
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy
+
+    def add_block(self):
+        body_copy = self.body[:]
+        body_copy.insert(0, body_copy[0] + self.direction)
+        self.body = body_copy
+
+    def check_death(self):
+        if self.body[0].x > 38 or self.body[0].x < 1 or self.body[0].y > 38 or self.body[0].y < 1:
+            return True
+        elif self.body[0] in self.body[1:] and self.direction != Vector2(0, 0):
+            return True
+        else:
+            return False
+
+    def update_head(self):
+        if self.direction == Vector2(1, 0):
+            self.head = self.head_right
+        elif self.direction == Vector2(-1, 0):
+            self.head = self.head_left
+        elif self.direction == Vector2(0, 1):
+            self.head = self.head_down
+        elif self.direction == Vector2(0, -1):
+            self.head = self.head_up
+
+    def update_tail(self):
+        tail_relation = self.body[-1] - self.body[-2]
+        if tail_relation == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_relation == Vector2(0, -1):
+            self.tail = self.tail_down
--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import random
 import pygame
 import colors
+import classes
+from pygame.math import Vector2
 
 # initialize pygame
 pygame.init()
 
 # game variables
-cell_size = 40
-cell_number = 20
-gameWindow = pygame.display.set_mode((cell_size*cell_number, cell_size*cell_number))
+cell_size = 20
+cell_number = 40
+gameWindow = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 
 clock = pygame.time.Clock()
 
 font = pygame.font.SysFont(None, 55)
+
+
+class Main:
+    def __init__(self):
+        self.snake = classes.Snake()
+        self.food = classes.Food()
+        self.score = 0
+        bg_img = pygame.image.load("snakebg.jpg")
+        self.bg_img = pygame.transform.scale(bg_img, (cell_number*cell_size, cell_number*cell_size))
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+
+    def draw_stuff(self):
+        gameWindow.blit(self.bg_img, (0,0))
+        self.snake.draw_snake(gameWindow)
+        self.food.draw_food(gameWindow)
+        text_screen(f"Score {self.score}", colors.red, 300, 80)
+
+    def check_collision(self):
+        if self.food.pos == self.snake.body[0]:
+            self.food = classes.Food()
+            self.snake.add_block()
+            self.score += 1
 
 
 # function to print text on screen
@@ -22,30 +49,47 @@ def text_screen(text, color, x, y):
 
 
 # main game loop
-def game_loop():
-    # loop variables
-    game_quite = False
-    game_over = False
-    score = 0
-    score_color = colors.red
 
-    food_x = random.randint(50, 400)  # x and y coordinates of food
-    food_y = random.randint(50, 250)
-    food_size = 25  # size of food
-    food_col = colors.green  # this represents green color
-    accuracy = 10  # how close the snake must be to eat the food
+# loop variables
+game_quite = False
+game_over = False
+score = 0
+score_color = colors.red
+fps = 60
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
 
-    fps = 100
+main_game = Main()
 
-    while not game_quite:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+while not game_quite:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_quite = True
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+            game_over = main_game.snake.check_death()
+            if game_over:
+                main_game = Main()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                if main_game.snake.direction.y == 0:
+                    main_game.snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_UP:
+                if main_game.snake.direction.y == 0:
+                    main_game.snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_LEFT:
+                if main_game.snake.direction.x == 0:
+                    main_game.snake.direction = Vector2(-1, 0)
+            if event.key == pygame.K_RIGHT:
+                if main_game.snake.direction.x == 0:
+                    main_game.snake.direction = Vector2(1, 0)
+            if event.key == pygame.K_SPACE:
                 game_quite = True
-                pygame.quit()
-                quit()
-        gameWindow.fill(colors.white)
-        pygame.display.update()
-        clock.tick(fps)
 
+    main_game.draw_stuff()
 
-game_loop()
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.quit()
+quit()
